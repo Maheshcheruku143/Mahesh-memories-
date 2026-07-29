d6b04583d33fe09a214fbe3e5eacad67723e9cb4
Mahesh Patel <125740911+Maheshcheruku143@users.noreply.github.com>
2026-07-29 11:19:03 +0530

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,20 @@
+function login(){
+
+const username=document.getElementById("username").value.trim();
+
+const password=document.getElementById("password").value.trim();
+
+if(username==="mahesh" && password==="Mahesh@2001"){
+
+alert("Welcome to Mahesh Memories ❤️");
+
+window.location.href="gallery.html";
+
+}
+else{
+
+alert("Wrong Username or Password");
+
+}
+
+}
